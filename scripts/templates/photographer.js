3066e900ae744befa425238dad18e7bbf926fb05
julien-ba <julienbarbier42@gmail.com>
2024-08-29 15:19:20 +0200

    completed userCardDom elements

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -43,13 +43,34 @@ export class PhotographerTemplate {
 
     async getUserCardDOM() {
         const article = document.createElement('article');
+
         const img = document.createElement('img');
         img.setAttribute('src', await this.getUserPicture())
         img.setAttribute('alt', `Portrait of ${this.getUserData().name}`);
+        img.classList.add('card-img');
+
         const h2 = document.createElement('h2');
         h2.textContent = this.getUserData().name;
+        h2.classList.add('card-title');
+
+        const h3 = document.createElement('h3');
+        h3.textContent = `${this.getUserData().city}, ${this.getUserData().country}`;
+        h3.classList.add('card-subtitle');
+
+        const p1 = document.createElement('p');
+        p1.textContent = this.getUserData().tagline;
+        p1.classList.add('card-description');
+
+        const p2 = document.createElement('p');
+        p2.textContent = `${this.getUserData().price}€/jour`;
+        p2.classList.add('card-price');
+
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        article.appendChild(h3);
+        article.appendChild(p1);
+        article.appendChild(p2);
+
+        return article;
     }
 }
