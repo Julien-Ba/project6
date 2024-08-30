1bb1719e996bdab25693c33129dcfc4211b97cf0
julien-ba <julienbarbier42@gmail.com>
2024-08-30 09:32:20 +0200

    moved fetch to api + simplified code

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,39 +1,21 @@
+import { getPhotographers } from '../api/fetch_data.js';
 import { PhotographerTemplate } from '../templates/photographer.js';
 
 
 
-async function getPhotographers() {
-    const src = 'data/photographers.json';
-    try {
-        const response = await fetch(src);
-        if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-        }
-
-        const json = await response.json();
-        console.debug(json);
-        return json;
-    } catch (error) {
-        console.error(error.message);
-    }
-}
-
-async function displayData(photographers) {
+async function displayUserCardDOM(photographers) {
     const photographersSection = document.querySelector('.photographer_section');
 
-    photographers.forEach((photographer) => {
+    photographers.forEach(async (photographer) => {
         const photographerModel = new PhotographerTemplate(photographer);
-        photographerModel.getUserCardDOM().then(cardDOM => {
-            photographersSection.appendChild(cardDOM);
-        }).catch(error => {
-            console.error('Error creating photographer card:', error);
-        });
+        const cardDOM = await photographerModel.getUserCardDOM();
+        photographersSection.appendChild(cardDOM);
     });
 }
 
 async function init() {
     const { photographers } = await getPhotographers();
-    displayData(photographers);
+    displayUserCardDOM(photographers);
 }
 
 init();
