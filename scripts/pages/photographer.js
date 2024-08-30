b610987ba8562b14835478d7c0df839c1728510c
julien-ba <julienbarbier42@gmail.com>
2024-08-30 09:34:34 +0200

    add content to photographer

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,28 @@
-//Mettre le code JavaScript lié à la page photographer.html
+import { getPhotographers } from '../api/fetch_data.js';
+import { PhotographerTemplate } from '../templates/photographer.js';
+
+
+
+async function populatePhotographer(photographers) {
+    const photographerHeader = document.querySelector('.photograph-header');
+
+    photographers.forEach(async (photographer) => {
+        const id = new URLSearchParams(window.location.search).get('id');
+        if (photographer.id != id) return;
+
+        const photographerModel = new PhotographerTemplate(photographer);
+
+        const infoDOM = photographerModel.getUserInfoDOM();
+        photographerHeader.insertAdjacentElement('afterbegin', infoDOM);
+
+        const pictureDOM = await photographerModel.getUserImgDOM();
+        photographerHeader.insertAdjacentElement('beforeend', pictureDOM);
+    });
+}
+
+async function init() {
+    const { photographers } = await getPhotographers();
+    populatePhotographer(photographers);
+}
+
+init();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,4 +1,4 @@
-import { PhotographerData } from "../api/photographer_data.js";
+import { PhotographerData } from "../api/verify_data.js";
 
 
 export class PhotographerTemplate extends PhotographerData {
@@ -15,16 +15,27 @@ export class PhotographerTemplate extends PhotographerData {
         link.href = `./photographer.html?id=${this.getUserData().id}`;
         article.appendChild(link);
 
-        link.appendChild(await this.createImg());
-        link.appendChild(this.createTitle());
-        link.appendChild(this.createSubtitle());
-        link.appendChild(this.createDescription());
-        link.appendChild(this.createPrice());
+        link.appendChild(await this.getUserImgDOM());
+        link.appendChild(this.getUserNameDOM());
+        link.appendChild(this.getUserLocationDOM());
+        link.appendChild(this.getUserTaglineDOM());
+        link.appendChild(this.getUserPriceDOM());
 
         return article;
     }
 
-    async createImg() {
+    getUserInfoDOM() {
+        const div = document.createElement('div');
+        div.classList.add('user-info');
+
+        div.appendChild(this.getUserNameDOM());
+        div.appendChild(this.getUserLocationDOM());
+        div.appendChild(this.getUserTaglineDOM());
+
+        return div;
+    }
+
+    async getUserImgDOM() {
         const img = document.createElement('img');
         img.src = await this.getUserPicture();
         img.alt = `Portrait of ${this.getUserData().name}`;
@@ -33,28 +44,28 @@ export class PhotographerTemplate extends PhotographerData {
         return img;
     }
 
-    createTitle() {
+    getUserNameDOM() {
         const h2 = document.createElement('h2');
         h2.textContent = this.getUserData().name;
         h2.classList.add('card-title');
         return h2;
     }
 
-    createSubtitle() {
+    getUserLocationDOM() {
         const h3 = document.createElement('h3');
         h3.textContent = `${this.getUserData().city}, ${this.getUserData().country}`;
         h3.classList.add('card-subtitle');
         return h3;
     }
 
-    createDescription() {
+    getUserTaglineDOM() {
         const p = document.createElement('p');
         p.textContent = this.getUserData().tagline;
         p.classList.add('card-description');
         return p;
     }
 
-    createPrice() {
+    getUserPriceDOM() {
         const p = document.createElement('p');
         p.textContent = `${this.getUserData().price}€/jour`;
         p.classList.add('card-price');
