8efbb8983169284e647a475ceaa29bc7907befa2
julien-ba <julienbarbier42@gmail.com>
2024-09-05 15:42:37 +0200

    moved focus trap to api, added focus to the close img + event to simulate click with enter

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,24 +1,29 @@
+import { simulateClick, trapFocus } from "../api/accessibility.js";
+
+
+
 const modal = document.getElementById("contact_modal");
-const focusableEls = modal.querySelectorAll('input, textarea');
+const focusableEls = modal.querySelectorAll('input, textarea, .close-modal-btn');
 
 const openModalBtn = document.querySelector('.contact_button');
 openModalBtn.addEventListener('click', displayModal);
 
 const closeModalBtn = document.querySelector('.close-modal-btn');
 closeModalBtn.addEventListener('click', closeModal);
+closeModalBtn.addEventListener('keydown', simulateClick);
 modal.addEventListener('keydown', escModal);
 
 const submitForm = document.querySelector('form');
 submitForm.addEventListener('submit', validateForm);
 
-modal.addEventListener('keydown', tabInModal);
+modal.addEventListener('keydown', event => trapFocus(event, focusableEls));
 
 
 
 function displayModal() {
     modal.style.display = 'block';
     modal.ariaHidden = 'false';
-    focusableEls[0].focus();
+    focusableEls[1].focus();
 }
 
 function closeModal() {
@@ -28,27 +33,13 @@ function closeModal() {
 
 function escModal(event) {
     // Close modal when escape key is pressed
-    const escKey = 27;
-    if (modal.ariaHidden === 'false' && event.keyCode === escKey)
+    const isEscPressed = (event.key === 'Escape' || event.keyCode === 27);
+    if (modal.ariaHidden === 'false' && isEscPressed)
         closeModal();
 }
 
 function validateForm(event) {
     event.preventDefault();
-    event.target.reset();
-    closeModal();
-}
-
-function tabInModal(event) {
-    const tabKey = 9;
-    if (event.keyCode !== tabKey) return;
-    if (event.shiftKey && document.activeElement === focusableEls[0]) {
-        focusableEls[focusableEls.length - 1].focus();
-        event.preventDefault();
-    } else {
-        if (document.activeElement === focusableEls[focusableEls.length - 1]) {
-            focusableEls[0].focus();
-            event.preventDefault();
-        }
-    }
+    //event.target.reset();
+    //closeModal();
 }
