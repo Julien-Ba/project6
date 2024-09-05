9635b871a462d4c562def9c2809d061c92788d48
julien-ba <julienbarbier42@gmail.com>
2024-09-05 10:40:21 +0200

    event to close modal with esc

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,22 +1,31 @@
+const modal = document.getElementById("contact_modal");
+
 const openModalBtn = document.querySelector('.contact_button');
 openModalBtn.addEventListener('click', displayModal);
 
 const closeModalBtn = document.querySelector('.close-modal-btn');
 closeModalBtn.addEventListener('click', closeModal);
+document.addEventListener('keydown', escModal);
 
 const submitForm = document.querySelector('form');
 submitForm.addEventListener('submit', validateForm);
 
+
+
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
     modal.style.display = 'block';
     modal.ariaHidden = 'false';
 }
 
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
     modal.style.display = 'none';
     modal.ariaHidden = 'true';
+}
+
+function escModal(event) {
+    // Close modal when escape key is pressed
+    if (modal.ariaHidden === 'false' && event.keyCode === 27)
+        closeModal();
 }
 
 function validateForm(event) {
