6990ece983dbdb38f9db048278af9a617d495cf7
julien-ba <julienbarbier42@gmail.com>
2024-09-05 11:45:49 +0200

    control lightbox with keyboard

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,29 +1,38 @@
+const lightbox = document.querySelector('.background');
+
 document.addEventListener('click', openLightbox);
 
 const closeLightboxBtn = document.querySelector('.lightbox-btn > .fa-x');
 closeLightboxBtn.addEventListener('click', closeLightbox);
+document.addEventListener('keydown', escLightbox);
 
 const switchLightboxBtns = document.querySelectorAll('.switch-lightbox');
 switchLightboxBtns.forEach(btn => btn.addEventListener('click', switchLightbox));
+document.addEventListener('keydown', switchLightbox);
 
 function openLightbox(event) {
     const media = event.target;
     if (!media.classList.contains('card-img') || !media.parentNode.classList.contains('card')) return;
-    const container = document.querySelector('.background');
-    container.dataset.lightbox_opened = 'true';
-    container.ariaHidden = 'false';
+    lightbox.dataset.lightbox_opened = 'true';
+    lightbox.ariaHidden = 'false';
     media.dataset.lightbox_focus = 'true';
 }
 
 function closeLightbox() {
-    const container = document.querySelector('.background');
-    container.dataset.lightbox_opened = 'false';
-    container.ariaHidden = 'true';
+    lightbox.dataset.lightbox_opened = 'false';
+    lightbox.ariaHidden = 'true';
     document.querySelectorAll('.card > .card-img').forEach(img => img.dataset.lightbox_focus = 'false');
 }
 
+function escLightbox(event) {
+    // Close lightbox when escape key is pressed
+    if (lightbox.ariaHidden === 'false' && event.keyCode === 27)
+        closeLightbox();
+}
+
 function switchLightbox(event) {
-    const side = event.target;
+    const side = switchSide(event);
+    if (side == null) return;
     const medias = document.querySelectorAll('.media-container .card-img');
     const len = medias.length;
     let index = 0;
@@ -37,11 +46,22 @@ function switchLightbox(event) {
 
     medias[index].dataset.lightbox_focus = 'false';
 
-    if (side.classList.contains('btn-previous')) {
+    if (side === 'left') {
         if (index === 0) index = len;
         medias[index - 1].dataset.lightbox_focus = 'true';
     } else {
         if (index === len - 1) index = -1;
         medias[index + 1].dataset.lightbox_focus = 'true';
     }
+}
+
+function switchSide(event) {
+    //check if a switch button has been clicked or the keys left/right have been pressed
+    // undefined allow clicks
+    if (lightbox.ariaHidden === 'false' && event.keyCode !== undefined && event.keyCode !== 37 && event.keyCode !== 39 || event.keyCode === 27)
+        return;
+    let side = 'right';
+    if (event.target.classList.contains('btn-previous') || event.keyCode === 37)
+        side = 'left';
+    return side;
 }
