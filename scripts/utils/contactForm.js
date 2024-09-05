c66650dfe242bb9066606da0496aa980e72866ba
julien-ba <julienbarbier42@gmail.com>
2024-09-05 13:33:03 +0200

    added focus trap

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,20 +1,24 @@
 const modal = document.getElementById("contact_modal");
+const focusableEls = modal.querySelectorAll('input, textarea');
 
 const openModalBtn = document.querySelector('.contact_button');
 openModalBtn.addEventListener('click', displayModal);
 
 const closeModalBtn = document.querySelector('.close-modal-btn');
 closeModalBtn.addEventListener('click', closeModal);
-document.addEventListener('keydown', escModal);
+modal.addEventListener('keydown', escModal);
 
 const submitForm = document.querySelector('form');
 submitForm.addEventListener('submit', validateForm);
+
+modal.addEventListener('keydown', tabInModal);
 
 
 
 function displayModal() {
     modal.style.display = 'block';
     modal.ariaHidden = 'false';
+    focusableEls[0].focus();
 }
 
 function closeModal() {
@@ -24,7 +28,8 @@ function closeModal() {
 
 function escModal(event) {
     // Close modal when escape key is pressed
-    if (modal.ariaHidden === 'false' && event.keyCode === 27)
+    const escKey = 27;
+    if (modal.ariaHidden === 'false' && event.keyCode === escKey)
         closeModal();
 }
 
@@ -32,4 +37,18 @@ function validateForm(event) {
     event.preventDefault();
     event.target.reset();
     closeModal();
+}
+
+function tabInModal(event) {
+    const tabKey = 9;
+    if (event.keyCode !== tabKey) return;
+    if (event.shiftKey && document.activeElement === focusableEls[0]) {
+        focusableEls[focusableEls.length - 1].focus();
+        event.preventDefault();
+    } else {
+        if (document.activeElement === focusableEls[focusableEls.length - 1]) {
+            focusableEls[0].focus();
+            event.preventDefault();
+        }
+    }
 }
