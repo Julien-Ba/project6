7e0dab7dffb8aef53ab6eb51091f92e909f00e2f
julien-ba <julienbarbier42@gmail.com>
2024-09-11 10:29:17 +0200

    check if isNaN

--- scripts/utils/counterLikes.js
+++ scripts/utils/counterLikes.js
@@ -9,15 +9,24 @@ function likesCounter(event) {
     const totalLikes = document.querySelector('.total-likes');
 
     if (!likes.dataset.liked || likes.dataset.liked === 'false') {
-        likes.textContent = (parseInt(likes.textContent) + 1).toString();
-        totalLikes.textContent = (parseInt(totalLikes.textContent) + 1).toString();
+        isLikeNumber(event, 0);
+        likes.textContent = (Number(likes.textContent) + 1).toString();
+        totalLikes.textContent = (Number(totalLikes.textContent) + 1).toString();
         likes.dataset.liked = 'true';
         return;
     }
 
     if (likes.dataset.liked === 'true') {
-        likes.textContent = (parseInt(likes.textContent) - 1).toString();
-        totalLikes.textContent = (parseInt(totalLikes.textContent) - 1).toString();
+        isLikeNumber(event, 1);
+        likes.textContent = (Number(likes.textContent) - 1).toString();
+        totalLikes.textContent = (Number(totalLikes.textContent) - 1).toString();
         likes.dataset.liked = 'false';
+    }
+}
+
+function isLikeNumber(event, resetPoint) {
+    if (Number.isNaN(event.target.textContent)) {
+        console.warn(`The ${event.target} was containing ${event.target.textContent}, NOT a valid value, resetting to ${resetPoint} ...`);
+        event.target.textContent = `${resetPoint}`;
     }
 }
